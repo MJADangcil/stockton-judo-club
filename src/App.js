4ee9e9ac8c5c347538fb7d9266bfd3c75e174ad8
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2021-11-17 15:26:53 -0800

    Updated pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import HomePage from './pages/Home';
+import HomePage from './pages/HomePage';
 import ClassSchedule from './pages/ClassSchedule';
 import Membership from './pages/Membership';
 import Instructors from './pages/Instructors';
