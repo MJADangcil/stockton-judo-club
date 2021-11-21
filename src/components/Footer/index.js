2d509425bdc2b0ef020f8e33eb132ccd61248e13
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2021-11-20 17:33:15 -0800

    Updated Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
-// import Box from '@mui/material/Box';
-// import Grid from '@mui/material/Grid';
-// import Container from '@mui/material/Container';
-// import EmailIcon from '@mui/icons-material/Email';
-// import FacebookIcon from '@mui/icons-material/Facebook';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import EmailIcon from '@mui/icons-material/Email';
 
 function Footer() {
   return (
     <footer>
-      footer
+      <AppBar
+        position='fixed'
+        color='secondary'
+        sx={{
+          backgroundColor: '#292929',
+          top: 'auto',
+          bottom: 0,
+        }}
+      >
+        <Toolbar>
+          <Box sx={{ width: '100%' }}>
+            <Grid
+              container
+              alignItems='center'
+              justifyContent='center'
+              rowSpacing={0.5}
+              spacing={2}
+            >
+              <Grid item component='h5' align='center'>
+                STOCKTON JUDO CLUB
+              </Grid>
+              <Grid item align='center'>
+                <FacebookIcon />
+              </Grid>
+              <Grid item align='center'>
+                <EmailIcon />
+              </Grid>
+              <Grid item component='span' align='center'>
+                All rights reserved.
+              </Grid>
+            </Grid>
+          </Box>
+        </Toolbar>
+      </AppBar>
     </footer>
   );
 }
