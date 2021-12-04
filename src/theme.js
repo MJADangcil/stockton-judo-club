0e868444b6b4ea0d9f7a065cbb4f52e6e0652515
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2021-12-03 17:03:08 -0800

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -6,11 +6,13 @@ export const theme = createTheme({
       main: '#c62828',
       light: '#ff5f52',
       dark: '#8e0000',
+      text: '#ffffff',
     },
     secondary: {
       main: '#283593',
       light: '#5f5fc4',
       dark: '#001064',
+      text: '#ffffff',
     },
   },
 });
