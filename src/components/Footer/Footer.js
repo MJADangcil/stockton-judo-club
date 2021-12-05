ae565a40bb6cfa4177d61fcdc1d33eab88b5f84e
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2021-12-04 17:14:20 -0800

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,63 +1,60 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, IconButton } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 import FacebookIcon from '@mui/icons-material/Facebook';
-import { IconButton } from '@mui/material';
 
 function Footer() {
   return (
-    <footer>
-      <Container
+    <Container
+      component='footer'
+      maxWidth={false}
+      sx={{
+        backgroundColor: 'secondary.main',
+        color: 'secondary.text',
+        bottom: 0,
+        position: 'absolute',
+      }}
+    >
+      <Grid
         component='div'
-        maxWidth={false}
+        container
+        columnGap={2}
         sx={{
-          backgroundColor: 'secondary.main',
-          color: 'secondary.text',
-          bottom: 0,
-          position: 'absolute',
+          flexDirection: { xs: 'column', sm: 'row' },
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <Grid
-          component='div'
-          container
-          columnGap={2}
+          component='span'
+          item
           sx={{
-            flexDirection: { xs: 'column', sm: 'row' },
-            justifyContent: 'center',
-            alignItems: 'center',
+            textAlign: 'center',
+            fontSize: '14px',
+            paddingTop: { xs: '8px', sm: '0px' },
           }}
         >
-          <Grid
-            component='span'
-            item
-            sx={{
-              textAlign: 'center',
-              fontSize: '14px',
-              paddingTop: { xs: '8px', sm: '0px' },
-            }}
-          >
-            {new Date().getFullYear()} Stockton Judo Club. All rights reserved.
-          </Grid>
-          <Grid component='div' item>
-            <IconButton
-              component='button'
-              aria-label='Stockton Judo contact email'
-              sx={{ color: 'secondary.text' }}
-            >
-              <EmailIcon />
-            </IconButton>
-            <IconButton
-              component='button'
-              title='Stockton Judo Facebook page'
-              aria-label='Stockton Judo Facebook page'
-              sx={{ color: 'secondary.text' }}
-            >
-              <FacebookIcon />
-            </IconButton>
-          </Grid>
+          {new Date().getFullYear()} Stockton Judo Club. All rights reserved.
         </Grid>
-      </Container>
-    </footer>
+        <Grid component='div' item>
+          <IconButton
+            component='button'
+            aria-label='Stockton Judo contact email'
+            sx={{ color: 'secondary.text' }}
+          >
+            <EmailIcon />
+          </IconButton>
+          <IconButton
+            component='button'
+            title='Stockton Judo Facebook page'
+            aria-label='Stockton Judo Facebook page'
+            sx={{ color: 'secondary.text' }}
+          >
+            <FacebookIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
