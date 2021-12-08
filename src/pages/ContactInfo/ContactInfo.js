f9fb5efcdbf1f1f02eb2831132ac1ee85d4543dc
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2021-12-07 18:53:50 -0800

    Update ContactInfo.js

--- src/pages/ContactInfo/ContactInfo.js
+++ src/pages/ContactInfo/ContactInfo.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { Container, Divider, Grid, Link, Typography } from '@mui/material';
 
 export default function ContactInfo() {
-  return <h1>Contact Info</h1>;
+  return (
+    <section>
+      <Container maxWidth={false} sx={{ display: 'table-row', height: 'auto' }}>
+        <Grid container rowSpacing={2.5} sx={{ padding: '16px' }}>
+          <Grid item xs={12}>
+            <Typography variant='h4'>Contact Info</Typography>
+            <Divider />
+          </Grid>
+          <Grid item>
+            <Typography variant='h6'>
+              Schedule: Monday, Tuesday, and Thursday, 6:30pm to 8:00pm PST
+            </Typography>
+            <Typography variant='h6'>
+              Email:{' '}
+              <Link href='mailto:stocktonjudo@yahoo.com'>
+                stocktonjudo@yahoo.com
+              </Link>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  );
 }
