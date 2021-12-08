482f61b35298e5139be55cafcb2cbd67b71ecb2a
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2021-12-08 15:32:53 -0800

    Update ContactInfo.js

--- src/pages/ContactInfo/ContactInfo.js
+++ src/pages/ContactInfo/ContactInfo.js
@@ -1,5 +1,48 @@
 import React from 'react';
-import { Container, Divider, Grid, Link, Typography } from '@mui/material';
+import {
+  Container,
+  Divider,
+  Grid,
+  Link,
+  Paper,
+  TableContainer,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Typography,
+} from '@mui/material';
+
+const schedule = [
+  {
+    day: 'Sunday',
+    hours: 'Closed',
+  },
+  {
+    day: 'Monday',
+    hours: '6:30 to 8:00pm',
+  },
+  {
+    day: 'Tuesday',
+    hours: '6:30 to 8:00pm',
+  },
+  {
+    day: 'Wednesday',
+    hours: 'Closed',
+  },
+  {
+    day: 'Thursday',
+    hours: '6:30 to 8:00pm',
+  },
+  {
+    day: 'Friday',
+    hours: 'Closed',
+  },
+  {
+    day: 'Saturday',
+    hours: 'Closed',
+  },
+];
 
 export default function ContactInfo() {
   return (
@@ -8,11 +51,11 @@ export default function ContactInfo() {
         <Grid
           container
           rowGap={3}
-          columnGap={3}
+          columnGap={2}
           sx={{
             padding: '16px',
             justifyContent: 'center',
-            alignItems: 'center',
+            alignItems: 'flex-start',
           }}
         >
           <Grid item xs={12}>
@@ -20,17 +63,52 @@ export default function ContactInfo() {
               <Typography variant='h4'>Contact Info</Typography>
             </Divider>
           </Grid>
-          <Grid item xs={12} sm={5}>
+          <Grid item xs={12} sm={5} lg={3}>
             <Typography variant='h5'>Schedule</Typography>
-            <Typography varient='body1'>
-              Monday, Tuesday, and Thursday, 6:30pm-8:00pm PST
-            </Typography>
+            <TableContainer component={Paper} raised={true}>
+              <Table>
+                <TableBody>
+                  {schedule.map((s, i) => (
+                    <TableRow key={i}>
+                      <TableCell component='td' scope='row'>
+                        {s.day}
+                      </TableCell>
+                      <TableCell component='td' scope='row' align='right'>
+                        {s.hours}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Grid>
-          <Grid item xs={12} sm={5}>
+          <Grid item xs={12} sm={5} lg={2}>
             <Typography variant='h5'>Email</Typography>
             <Typography variant='body1'>
               <Link href='mailto:stocktonjudo@yahoo.com'>
                 stocktonjudo@yahoo.com
+              </Link>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={5} lg={2}>
+            <Typography variant='h5'>Facebook</Typography>
+            <Typography variant='body1'>
+              <Link
+                href='https://www.facebook.com/StocktonJudoClub/'
+                target='_blank'
+              >
+                Stockton Judo Club
+              </Link>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={5} lg={2}>
+            <Typography variant='h5'>Address</Typography>
+            <Typography variant='body1'>
+              <Link
+                href='https://goo.gl/maps/XdtFPYiMBb56Zhe96'
+                target='_blank'
+              >
+                424 E 9th St, Stockton, CA 95206
               </Link>
             </Typography>
           </Grid>
