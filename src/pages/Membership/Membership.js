67e61d28b138ac5ef59ba96335b47d2bd91b8d07
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2021-12-07 18:09:01 -0800

    Update Membership.js

--- src/pages/Membership/Membership.js
+++ src/pages/Membership/Membership.js
@@ -1,38 +1,92 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material';
+import {
+  Container,
+  Divider,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Link,
+} from '@mui/material';
+
+const membershipCosts = [
+  {
+    student: '1 student',
+    qualifier: undefined,
+    due: '$70/month',
+  },
+  {
+    student: '2 students',
+    qualifier: 'from same household',
+    due: '$90/month',
+  },
+  {
+    student: '3+ students',
+    qualifier: 'from same household',
+    due: '$110/month',
+  },
+];
 
 export default function Membership() {
   return (
     <section>
       <Container maxWidth={false} sx={{ display: 'table-row', height: 'auto' }}>
-        <Grid container rowSpacing={2.5} sx={{ padding: '16px' }}>
-          <Grid component='h3' item xs={12}>
-            Membership
+        <Grid
+          container
+          rowSpacing={2.5}
+          columnGap={3}
+          sx={{
+            padding: '16px',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+          }}
+        >
+          <Grid item xs={12}>
+            <Typography variant='h4'>Membership</Typography>
+            <Divider />
+            <Typography variant='overline'>
+              (rates current as of January 1, 2022)
+            </Typography>
           </Grid>
-          <Grid component='span' item xs={12}>
-            (rates current as of January 1, 2022)
+          {membershipCosts.map((membership, i) => (
+            <Grid
+              key={i}
+              component={Card}
+              raised={true}
+              // item
+              sx={{
+                margin: '16px',
+                width: '200px',
+              }}
+            >
+              <CardContent>
+                <Typography variant='h5'>{membership.student}</Typography>
+                <Typography variant='caption'>
+                  {membership.qualifier}
+                  <Divider />
+                </Typography>
+                <Typography variant='body1'>{membership.due}</Typography>
+              </CardContent>
+            </Grid>
+          ))}
+          <Grid item xs={12}>
+            <Typography variant='body1'>
+              Members must also join the United States Judo Federation (USJF).
+              Individual memberships to the USJF are $70 per year, with family
+              memberships available for families of three or more. Go to{' '}
+              <Link href='https://www.usjf.com/registration/' target='_blank'>
+                https://www.usjf.com/registration/
+              </Link>{' '}
+              for more information or to register online.
+            </Typography>
           </Grid>
-          <Grid component='span' item xs={12}>
-            Individual - $70 per month
-          </Grid>
-          <Grid component='span' item xs={12}>
-            2 from same household - $90 per month
-          </Grid>
-          <Grid component='span' item xs={12}>
-            3 or more from same household - $110 per month
-          </Grid>
-          <Grid component='span' item xs={12}>
-            Members must also join the United States Judo Federation (USJF).
-            Individual memberships to the USJF are $70 per year, with family
-            memberships available for families of three or more. Go to
-            https://www.usjf.com/registration/ for more information or to
-            register online.
-          </Grid>
-          <Grid component='span' item xs={12}>
-            Stockton Judo Club charges a $10 per session mat fee for guests, and
-            guests must show proof of membership in the United States Judo
-            Federation, United States Judo Association, or US Judo, Incorporated
-            prior to stepping on the mat.
+          <Grid item xs={12}>
+            <Typography variant='body1'>
+              Stockton Judo Club charges a $10 per session mat fee for guests,
+              and guests must show proof of membership in the United States Judo
+              Federation, United States Judo Association, or US Judo,
+              Incorporated prior to stepping on the mat.
+            </Typography>
           </Grid>
         </Grid>
       </Container>
