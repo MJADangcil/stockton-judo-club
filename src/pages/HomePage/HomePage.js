01ede6a298c62fbd427460b893fc9b83a11b4375
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2023-01-07 13:27:29 -0800

    Added HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,52 @@
-import { Container, Divider, Grid, Link, Typography } from '@mui/material';
-import React from 'react';
+import { Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import logo from "../../assets/logo_large.png";
 
 export default function HomePage() {
   return (
     <section>
-      HomePage
+      <Container
+        maxWidth={false}
+        sx={{
+          display: "block",
+          position: "absolute",
+          textAlign: "center",
+          top: "20%",
+        }}
+      >
+        <Grid
+          container
+          direction="column"
+          rowGap={8}
+          sx={{
+            padding: "16px",
+          }}
+        >
+          <Grid item>
+            <img
+              src={logo}
+              alt="Stockton Judo Club logo"
+              style={{
+                height: "300px",
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">Welcome to Stockton Judo Club</Typography>
+            <Typography variant="h6">
+              Established 1946 Member – Daiheigen Yudanshakai & United States
+              Judo Federation
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">
+              When confronted by an attacker, hit your adversary with the
+              biggest thing available:
+            </Typography>
+            <Typography variant="h6">THE GROUND</Typography>
+          </Grid>
+        </Grid>
+      </Container>
     </section>
   );
 }
