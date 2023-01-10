a88ce9f81bffeff02c7c83df9aa1497592d78a30
Michael Josh A. Dangcil <michaeljoshdangcil@gmail.com>
2023-01-09 16:58:54 -0800

    Update footer with quote changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,57 +1,58 @@
-import React from 'react';
-import { Container, Grid, IconButton } from '@mui/material';
-import EmailIcon from '@mui/icons-material/Email';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import React from "react";
+import { Container, Grid, IconButton, Button } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 export default function Footer() {
   return (
     <Container
-      component='footer'
+      component="footer"
       maxWidth={false}
       sx={{
-        display: 'table-row',
-        backgroundColor: 'secondary.main',
-        color: 'text.dark',
-        height: '1px',
+        display: "table-row",
+        backgroundColor: "secondary.main",
+        color: "text.dark",
+        height: "1px",
       }}
     >
       <Grid
-        component='div'
+        component="div"
         container
-        columnGap={2}
+        columnGap={3}
         sx={{
-          flexDirection: { xs: 'column', sm: 'row' },
-          justifyContent: 'center',
-          alignItems: 'center',
+          flexDirection: { xs: "column", sm: "row" },
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <Grid
-          component='span'
+          component="span"
           item
           sx={{
-            textAlign: 'center',
-            fontSize: '14px',
-            paddingTop: { xs: '8px', sm: '0px' },
+            textAlign: "center",
+            fontSize: "14px",
+            paddingTop: { xs: "8px", sm: "0px" },
           }}
         >
           {new Date().getFullYear()} Stockton Judo Club. All rights reserved.
         </Grid>
-        <Grid component='div' item>
+        <Grid component="div" item>
           <IconButton
-            component='button'
-            href='mailto:stocktonjudo@yahoo.com'
-            aria-label='Stockton Judo contact email'
-            sx={{ color: 'text.dark' }}
+            component="button"
+            href="mailto:stocktonjudo@yahoo.com"
+            title="Stockton Judo contact email"
+            aria-label="Stockton Judo contact email"
+            sx={{ color: "text.dark" }}
           >
             <EmailIcon />
           </IconButton>
           <IconButton
-            component='button'
-            href='https://www.facebook.com/StocktonJudoClub/'
-            target='_blank'
-            title='Stockton Judo Facebook page'
-            aria-label='Stockton Judo Facebook page'
-            sx={{ color: 'text.dark' }}
+            component="button"
+            href="https://www.facebook.com/StocktonJudoClub/"
+            target="_blank"
+            title="Stockton Judo Facebook page"
+            aria-label="Stockton Judo Facebook page"
+            sx={{ color: "text.dark" }}
           >
             <FacebookIcon />
           </IconButton>
